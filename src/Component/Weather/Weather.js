3894f7b7370766f1e630b4077f1b267cd4a558e5
jahidul <programmerjahid162@gmail.com>
2022-03-08 12:57:35 +0600

    adding weather app

---
+++ src/Component/Weather/Weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Weather = () => {
+  //     REACT_APP_API_URL = 'https://api.openweathermap.org/data/2.5'
+  // REACT_APP_API_KEY = ""
+  // REACT_APP_ICON_URL = 'https://openweathermap.org/img/w'
+  return <div></div>;
+};
+
+export default Weather;
